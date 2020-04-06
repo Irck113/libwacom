a56b3fb0618581cb4a14051e862eed35a3c9b58b
Irck113 <rick.mc.fly2@gmail.com>
2020-04-06 01:18:31 -0500

    First Commit.
    Provided by: https://github.com/linuxwacom/libwacom/wiki

---
+++ script.sh
@@ -0,0 +1,16 @@
+#Instalacion de dependencias
+sudo apt -y install build-essential && sudo apt -y build-dep libwacom
+sudo apt install -y libgudev-1.0-dev libxml++2.6-dev
+sudo apt install -y autoconf linux-headers-$(uname -r)
+sudo apt install -y libtool
+sudo apt install -y git
+
+#Cambio de directorio para clonar el repositorio
+cd ~/Downloads
+git clone https://github.com/linuxwacom/libwacom.git
+cd libwacom
+
+#Instalacion de los drivers mediante autogen
+./autogen.sh --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu/
+make
+sudo make install
