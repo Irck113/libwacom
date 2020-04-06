84aad82ca3f023e489f4125e986a0bb14a30e839
Irck113 <rick.mc.fly2@gmail.com>
2020-04-06 01:22:06 -0500

    Comment Git

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ sudo apt -y install build-essential && sudo apt -y build-dep libwacom
 sudo apt install -y libgudev-1.0-dev libxml++2.6-dev
 sudo apt install -y autoconf linux-headers-$(uname -r)
 sudo apt install -y libtool
-sudo apt install -y git
+#sudo apt install -y git
 
 #Cambio de directorio para clonar el repositorio
 cd ~/Downloads
